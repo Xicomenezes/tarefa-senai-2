06217b8f6e42b975d867ad9037540e5e2b136c7a
Carlos Eduardo <carlos.leitao@aluno.senai.br>
2022-07-29 10:01:17 -0400

    V0.0.2 atualização do cadastro de eventos

--- cadastro.js
+++ cadastro.js
@@ -1,25 +1,26 @@
 let lista = []
 
-let opcao = 's'
+var entrada = require('readline-sync');
+let opcao = 's';
 while (opcao == 's') {
-let nome = prompt("Coloque o seu nome:")
-let idade = prompt("Coloque a sua idade:")
-if (idade >= 18){
-    if(lista.length < 100){
-        console.log("Cadastro permitido!!!")
-        lista.push(nome)
+    let nome = prompt("Coloque o seu nome:");
+    let idade = prompt("Coloque a sua idade:");
+    if (idade >= 18) {
+        if (lista.length < 100) {
+            console.log("Cadastro permitido!!!");
+            lista.push(nome);
+        }
+        else
+            console.log("Cadastro não permitido, pois atingiu o limite máximo de 100.");
     }
-    else
-    console.log("Cadastro não permitido, pois atingiu o limite máximo de 100.")
-}
-else{
-    console.log("Cadastro não permitido pela idade.")
-}
-opcao = prompt("Cadastrar outro participante? (s ou n) ")
+    else {
+        console.log("Cadastro não permitido pela idade.");
+    }
+    opcao = prompt("Cadastrar outro participante? (s ou n) ");
 }
 
-i = 1 
-while (i < lista.length){
-    conosole.log(lista)
+i = 1
+while (i < lista.length) {
+    conosole.log(lista);
     i++
 }
