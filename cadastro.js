6e5c6b212fd916ac76ec47ec994b03f4b3c7fa27
Carlos Eduardo <carlos.leitao@aluno.senai.br>
2022-07-29 09:55:27 -0400

    V0.0.1 criação do sistema de cadastro

---
+++ cadastro.js
@@ -0,0 +1,25 @@
+let lista = []
+
+let opcao = 's'
+while (opcao == 's') {
+let nome = prompt("Coloque o seu nome:")
+let idade = prompt("Coloque a sua idade:")
+if (idade >= 18){
+    if(lista.length < 100){
+        console.log("Cadastro permitido!!!")
+        lista.push(nome)
+    }
+    else
+    console.log("Cadastro não permitido, pois atingiu o limite máximo de 100.")
+}
+else{
+    console.log("Cadastro não permitido pela idade.")
+}
+opcao = prompt("Cadastrar outro participante? (s ou n) ")
+}
+
+i = 1 
+while (i < lista.length){
+    conosole.log(lista)
+    i++
+}
